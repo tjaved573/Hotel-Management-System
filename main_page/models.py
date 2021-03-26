9e62c813a5c186ec92b5170c94ca84857fcd7c28
JeromeSchweitzer <jschweit@purdue.edu>
2021-03-26 11:13:09 -0400

    displaying hotel names

--- main_page/models.py
+++ main_page/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Hotel(models.Model):
+    hotel_id    = models.IntegerField(primary_key=True)
+    location    = models.CharField(max_length=100)
+    num_rooms   = models.IntegerField()
+    occupancy   = models.IntegerField()
+    rating      = models.DecimalField(max_digits=4, decimal_places=3)
--- main_page/views.py
+++ main_page/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from main_page.models import Hotel
 
 # Create your views here.
 #
 
 def main_page(request):
+    if "list_hotels" in request.POST:
+        hotels = []
+        for h in Hotel.objects.raw("SELECT * FROM hotel"):
+            hotels.append(h)
+        return render(request, 'main_page.html', {'hotels': hotels})    
     return render(request, 'main_page.html', {})
