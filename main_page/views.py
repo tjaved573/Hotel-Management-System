ad8e58e45c22da527b8b64361c0cbcf80988b893
JeromeSchweitzer <jschweit@purdue.edu>
2021-04-13 01:05:43 -0400

    login finished except for backend checks and redirection

--- main_page/views.py
+++ main_page/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import LoginForm
 
 def home(request):
     if "guest_login" in request.GET:
@@ -7,8 +8,22 @@ def home(request):
     return render(request, 'main_page/home.html', {})
 
 def login(request, entity):
+    form = LoginForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            cd = form.cleaned_data
+            print(f"username: {cd['username']}, password: {cd['password']}")
+            form = LoginForm()
+        else:
+            print("invalid form.")
+            if 'username' in form.errors:
+                print("username missing")
+            if 'password' in form.errors:
+                print("password missing")
+
     context = {
-        'entity': entity
+        'entity': entity,
+        'form': form
     }
     return render(request, 'main_page/login.html', context)
 
