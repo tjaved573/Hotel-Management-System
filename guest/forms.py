e22a9ad263809f797d3b26a37bf5fa3a2743bdd3
JeromeSchweitzer <jschweit@purdue.edu>
2021-05-01 22:08:22 -0400

    reservation forms displaying with room information

--- guest/forms.py
+++ guest/forms.py
@@ -2,17 +2,16 @@ from django import forms
 from guest.models import Hotel
 
 
-class HotelForm(forms.Form):
-    hotel = forms.ChoiceField(choices=[None])
+class ReservationForm(forms.Form):
     check_in_date = forms.DateField(input_formats=['%Y/%m/%d'])
     check_out_date = forms.DateField(input_formats=['%Y/%m/%d'])
-    rooms = forms.ChoiceField(choices=[(None, 'Please select a hotel')]) # TODO: get rooms associated with hotel
+    rooms = forms.ChoiceField(choices=[None])
     payment_type = forms.ChoiceField(choices=[('credit','credit'),('cash','cash')])
     Credit_card_number = forms.CharField(max_length=20)
 
-    def __init__(self, hotel_objs):
-        super(HotelForm, self).__init__()
-        self.fields['hotel'].choices = [(h, h.location) for h in hotel_objs]
+    def __init__(self, selected_room_bundle):
+        super(ReservationForm, self).__init__()
+        self.fields['rooms'].choices = selected_room_bundle
 
 
 
--- guest/views.py
+++ guest/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from guest.models import Guest, Reservation, ReservationRoomRel, Room, Hotel, Feature, FeatureRoomRel
-from .forms import HotelForm
+from .forms import ReservationForm
 
 
 def home(request, guest_id):
@@ -10,7 +10,7 @@ def home(request, guest_id):
     selected_reservation_id = None
     selected_res_info = None
     num_selected_rooms = None
-    if "get_reservation" in request.GET:
+    if "get_reservation" in request.GET:    # User has selected one of their reservations, get info for that reservation
         selected_reservation_id = int(request.GET.get('get_reservation'))
         print(f"\nTrying to get info for reservation {selected_reservation_id}\n")
 
@@ -56,15 +56,29 @@ def make_reservation(request, guest_id):
     guest = Guest.objects.get(guest_id=guest_id)
     hotels = Hotel.objects.all()
 
-    hotel_form = HotelForm(hotels)
-    if 'hotel_select' in request.POST:
-        hotel_form = HotelForm(request.POST)
+    selected_hotel_id = None
+    selected_room_bundle = [(None, 'If you are reading this than something has gone wrong')]
+    if "select_hotel" in request.GET:   # User has selected a hotel, get room data for that hotel to pass to form
+        selected_hotel_id = int(request.GET.get('select_hotel'))
+        selected_rooms = Room.objects.all().filter(hotel_id=selected_hotel_id)
+        selected_room_bundle = []
+        for room in selected_rooms:     # For each room, make a description that will be included in the form
+            room_feature_rel = FeatureRoomRel.objects.all().filter(room_id=room.room_id)
+            feature_names = [rel.feature.feature for rel in room_feature_rel]
+            room_desc_str = f"{room.room_type}: "
+            if len(feature_names) > 0:
+                room_desc_str += ', '.join(feature_names) + ", "
+            room_desc_str += f"${room.price_per_night} per night"
+            selected_room_bundle.append((room.room_id, room_desc_str))
+
+    reservation_form = ReservationForm(selected_room_bundle)    # Bundled array of (room_id, description) values -> make form
 
     context = {
         'guest_id': guest_id,
         'guest_name': f"{guest.first} {guest.last}",
-        'hotel_form': hotel_form,
-        # 'hotels': hotels,
+        'hotels': hotels,
+        'selected_hotel_id': selected_hotel_id,
+        'reservation_form': reservation_form,
     }
 
     return render(request, 'guest/make_reservation.html', context)
